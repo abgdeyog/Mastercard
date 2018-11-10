4a1ed98c156670444dad5fef5a9ace958b0953e1
abgdeyog <abgdeyog@gmail.com>
2018-11-10 05:40:00 +0300

    atms api

--- app/Atm.php
+++ app/Atm.php
@@ -9,7 +9,10 @@ class Atm extends Model
     protected $fillable = [
         'lat', 'lng', 'bank_id', 'operation', 'working_hours'
     ];
-
+//    protected $lat;
+//    protected $lgt;
+//    protected $bank_id;
+//    protected $operation;
     /**
      * The attributes that should be hidden for arrays.
      *
--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,6 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('atms', 'AtmController@index');
+Route::get('nearestAtms', 'AtmController@nearestAtms');
